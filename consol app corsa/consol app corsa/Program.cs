a0c4db07e5f202590aeada8a80413386b0e2c55f
Alberto Vincenzi <ALBERTO.VINCENZI@local.ittcesena.it>
2022-10-13 08:26:27 +0200

    1ver: 13/10

---
+++ consol app corsa/consol app corsa/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+              // Vincenzi  Alberto   4E    2022-09-22
+
+using System;
+using System.Threading;
+using static System.Console;
+
+namespace ConsolAppCorsa
+{
+    class program
+    {
+        static int posAndrea = 0;
+        static int posBaldo = 0;
+        static int posCarlo = 0;
+
+        static void Pronti()
+        {
+            SetCursorPosition(posAndrea, 2);
+            Write("Andrea");
+            SetCursorPosition(posAndrea, 3);
+            Write(" []");
+            SetCursorPosition(posAndrea, 4);
+            Write(@" /\");
+            SetCursorPosition(posAndrea, 5);
+            Write(@" /\");
+            SetCursorPosition(posBaldo, 6);
+            Write("Baldo");
+            SetCursorPosition(posAndrea, 7);
+            Write(@" /\");
+            SetCursorPosition(posAndrea, 8);
+            Write(@" /\");
+
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Vincenzi  Alberto   4E    2022-09-22");
+
+            CursorVisible = false;
+            //
+
+            Pronti();
+
+
+            ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+}
---
+++ prova visual studio 2/prova visual studio 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova_visual_studio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)    //  vincenzi  alberto     4E    2022-09-22     
+        {
+            Console.WriteLine("4E   Vincenzi  Alberto");
+            Console.WriteLine("ciao");
+            Console.WriteLine("hello");
+
+            Console.WriteLine("");
+
+        }
+    }
+}
